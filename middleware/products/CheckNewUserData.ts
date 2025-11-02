53b29bea4087bb1af707a65c7373188fe2df551a
MSLadanov <MSLadanov@gmail.com>
2025-11-02 20:54:38 +0300

    chore: :safety_vest: creating validate new user data middleware

--- middleware/products/CheckNewUserData.ts
+++ middleware/products/CheckNewUserData.ts
@@ -1,4 +1,15 @@
 import multer from "multer";
 import Express from 'express'
+import usersStorageConfig from "../../config/usersStorageConfig.ts";
 
-export const checkNewUserData = Express().use(multer({fileFilter: (req, file, cb) => {}}))
+export const checkNewUserData = Express().use(
+  multer({
+    fileFilter: (req, file, cb) => {
+    },
+    storage: usersStorageConfig,
+    limits: {
+      fileSize: 10 * 1024 * 1024,
+    },
+  }).fields([
+  ])
+);
