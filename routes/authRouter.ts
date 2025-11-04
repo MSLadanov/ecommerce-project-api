67d852aba35135883a87cd18b4eee0aaa44314e1
MSLadanov <MSLadanov@gmail.com>
2025-11-04 22:32:31 +0300

    chore: :safety_vest: creating validate new user data middleware

--- routes/authRouter.ts
+++ routes/authRouter.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import AuthController from "../controllers/AuthController.ts";
+import { checkNewUserData } from "../middleware/products/CheckNewUserData.ts";
 
 const authRouter = express.Router();
 
 authRouter.get("/login", AuthController.Login);
 
-authRouter.get("/register", AuthController.Register);
+authRouter.post("/register", checkNewUserData, AuthController.Register);
 
 authRouter.get("/me", AuthController.Me);
 
