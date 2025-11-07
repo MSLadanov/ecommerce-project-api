76c146704751db22331c2b09fbb0a8a9ccf757de
MSLadanov <MSLadanov@gmail.com>
2025-11-07 22:56:14 +0300

    chore: :safety_vest: creating validate new user data middleware

--- utils/UserSchema.ts
+++ utils/UserSchema.ts
@@ -1,6 +1,15 @@
 import z from "zod";
 
 export const UserSchema = z.object({
-    username: z.string(),
-    password: z.string(),
+  id: z.number(),
+  firstName: z.string(),
+  lastName: z.string(),
+  maidenName: z.string(),
+  age: z.number(),
+  gender: z.string(),
+  email: z.string(),
+  phone: z.string(),
+  username: z.string(),
+  password: z.string(),
+  birthDate: z.string(),
 });
